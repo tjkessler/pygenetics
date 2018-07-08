9ffbc2653ddccfb525257eb1f4fb0b3a4905604e
TJKessler <travis.j.kessler@gmail.com>
2018-07-08 15:19:46 -0400

    Update to version number

--- pygenetics/__init__.py
+++ pygenetics/__init__.py
@@ -1,3 +1,3 @@
 import pygenetics.ga_core
 import pygenetics.selection_functions
-__version__ = '0.2.0.dev1'
+__version__ = '0.2.1'
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name = 'pygenetics',
-version = "0.2.0.dev1",
+version = "0.2.1",
 description = 'Genetic algorithm framework for arbitrary cost and selection algorithms',
 url = 'http://github.com/tjkessler/pygenetics',
 author = 'Travis Kessler',
