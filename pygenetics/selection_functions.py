04fd64d5847bd816adac3d978fe2d8c34c5fa0e9
TJKessler <travis.j.kessler@gmail.com>
2018-07-08 15:19:32 -0400

    Added support for and example of maximizing best population members

--- pygenetics/selection_functions.py
+++ pygenetics/selection_functions.py
@@ -8,5 +8,10 @@
 #   Contains pre-built selection functions
 #
 
+# Select *n* *members* with the lowest fitness score
 def minimize_best_n(Members, n):
+    return(sorted(Members, key = lambda Member: Member.fitness_score)[0:n])
+
+# Select *n* *members* with the largest fitness score
+def maximize_best_n(Members, n):
     return(sorted(Members, key = lambda Member: Member.fitness_score)[0:n])
