39229106efcb48ada4de9860fd3a2e4878283afc
tjkessler <travis.j.kessler@gmail.com>
2018-09-12 19:40:18 -0400

    Updated to support multiprocessing

--- examples/example_maximize_integers.py
+++ examples/example_maximize_integers.py
@@ -2,7 +2,7 @@
 #   between 0 and 10 that result in the largest sum
 
 # Import Population object
-from pygenetics.ga_core import Population
+from pygenetics import Population
 # Import maximize selection function
 from pygenetics.selection_functions import maximize_best_n
 
@@ -15,30 +15,32 @@ def sum_of_integers(feed_dict):
         sum += feed_dict[integer]
     return sum
 
-# Initialize Population object with population size 10,
-#   cost function of 'sum_of_integers'
-pop = Population(10, sum_of_integers, select_fn=maximize_best_n)
+if __name__ == '__main__':
 
-# Add three integer parameters for each population member
-#   for the genetic algorithm to optimize (randomly
-#   initialized between 0 and 10)
-pop.add_parameter('first_integer', 0, 10)
-pop.add_parameter('second_integer', 0, 10)
-pop.add_parameter('third_integer', 0, 10)
+    # Initialize Population object with population size 10,
+    #   cost function of 'sum_of_integers'
+    pop = Population(10, sum_of_integers, select_fn=maximize_best_n)
 
-# Generate initial population with random parameter values
-pop.generate_population()
+    # Add three integer parameters for each population member
+    #   for the genetic algorithm to optimize (randomly
+    #   initialized between 0 and 10)
+    pop.add_parameter('first_integer', 0, 10)
+    pop.add_parameter('second_integer', 0, 10)
+    pop.add_parameter('third_integer', 0, 10)
 
-# Run the genetic algorithm for 15 generations
-num_generations = 15
-for generation in range(num_generations):
-    # Generate the next generation, using top 5 performers,
-    #   a mutation rate of 20%, and a maximum mutation amount
-    #   of 20% (0.2 * 10, 10 = param max - param min)
-    pop.next_generation(5, mut_rate=0.2, max_mut_amt=0.2)
+    # Generate initial population with random parameter values
+    pop.generate_population()
 
-# Print the fitness score and parameter values for each member
-for idx, member in enumerate(pop.members):
-    print('\nMember: ' + str(idx + 1))
-    print('Fitness Score: ' + str(member.fitness_score))
-    print('Parameters: ' + str(member.feed_dict))
+    # Run the genetic algorithm for 15 generations
+    num_generations = 15
+    for generation in range(num_generations):
+        # Generate the next generation, using top 5 performers,
+        #   a mutation rate of 20%, and a maximum mutation amount
+        #   of 20% (0.2 * 10, 10 = param max - param min)
+        pop.next_generation(5, mut_rate=0.2, max_mut_amt=0.2)
+
+    # Print the fitness score and parameter values for each member
+    for idx, member in enumerate(pop.members):
+        print('\nMember: ' + str(idx + 1))
+        print('Fitness Score: ' + str(member.fitness_score))
+        print('Parameters: ' + str(member.feed_dict))
--- examples/example_minimize_integers.py
+++ examples/example_minimize_integers.py
@@ -1,8 +1,8 @@
 # EXAMPLE SCRIPT: find the combination of 3 integers
-#   between 0 and 10 that result in the lowest sum
+#   between 0 and 10 that result in the smallest sum
 
 # Import Population object
-from pygenetics.ga_core import Population
+from pygenetics import Population
 
 
 # Cost function; finds the sum of integers in feed_dict
@@ -13,30 +13,32 @@ def sum_of_integers(feed_dict):
         sum += feed_dict[integer]
     return sum
 
-# Initialize Population object with population size 10,
-#   cost function of 'sum_of_integers'
-pop = Population(10, sum_of_integers)
+if __name__ == '__main__':
 
-# Add three integer parameters for each population member
-#   for the genetic algorithm to optimize (randomly
-#   initialized between 0 and 10)
-pop.add_parameter('first_integer', 0, 10)
-pop.add_parameter('second_integer', 0, 10)
-pop.add_parameter('third_integer', 0, 10)
+    # Initialize Population object with population size 10,
+    #   cost function of 'sum_of_integers'
+    pop = Population(10, sum_of_integers)
 
-# Generate initial population with random parameter values
-pop.generate_population()
+    # Add three integer parameters for each population member
+    #   for the genetic algorithm to optimize (randomly
+    #   initialized between 0 and 10)
+    pop.add_parameter('first_integer', 0, 10)
+    pop.add_parameter('second_integer', 0, 10)
+    pop.add_parameter('third_integer', 0, 10)
 
-# Run the genetic algorithm for 15 generations
-num_generations = 15
-for generation in range(num_generations):
-    # Generate the next generation, using top 5 performers,
-    #   a mutation rate of 20%, and a maximum mutation amount
-    #   of 20% (0.2 * 10, 10 = param max - param min)
-    pop.next_generation(5, mut_rate=0.2, max_mut_amt=0.2)
+    # Generate initial population with random parameter values
+    pop.generate_population()
 
-# Print the fitness score and parameter values for each member
-for idx, member in enumerate(pop.members):
-    print('\nMember: ' + str(idx + 1))
-    print('Fitness Score: ' + str(member.fitness_score))
-    print('Parameters: ' + str(member.feed_dict))
+    # Run the genetic algorithm for 15 generations
+    num_generations = 15
+    for generation in range(num_generations):
+        # Generate the next generation, using top 5 performers,
+        #   a mutation rate of 20%, and a maximum mutation amount
+        #   of 20% (0.2 * 10, 10 = param max - param min)
+        pop.next_generation(5, mut_rate=0.2, max_mut_amt=0.2)
+
+    # Print the fitness score and parameter values for each member
+    for idx, member in enumerate(pop.members):
+        print('\nMember: ' + str(idx + 1))
+        print('Fitness Score: ' + str(member.fitness_score))
+        print('Parameters: ' + str(member.feed_dict))
